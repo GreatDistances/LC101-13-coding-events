8987ed9caa461bb678663dc9ccc836e93d144b80
GreatDistances <117937385+GreatDistances@users.noreply.github.com>
2023-02-08 15:23:51 -0600

    2nd commit

---
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -0,0 +1,27 @@
+package org.launchcode.codingevents.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("events")
+public class EventController {
+
+    @GetMapping
+    public String events(Model model) {
+        List<String> events = new ArrayList<String>();
+        events.add("Coding Land");
+        events.add("Stack'em High");
+        events.add("404 But Not Atlanta");
+        //String events = "<ol>" + "<li>'Coding Land'</li>" + "<li>'Stack'em High'</li>" + "</ol>";
+
+        model.addAttribute("events", events);
+        return "events/index";
+    }
+
+}
