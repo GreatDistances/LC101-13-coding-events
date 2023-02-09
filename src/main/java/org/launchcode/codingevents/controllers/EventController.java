47bec01b512c23fdf5cabeb04d608dc6e183d147
GreatDistances <117937385+GreatDistances@users.noreply.github.com>
2023-02-08 20:41:52 -0600

    13.10 Exercises: Thymeleaf Views 1st

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -10,8 +10,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.ArrayList;
 import java.util.List;
 
-
-
+/**
+ * Created by Chris Bay
+ */
 @Controller
 @RequestMapping("events")
 public class EventController {
@@ -19,27 +20,22 @@ public class EventController {
     private static List<String> events = new ArrayList<>();
 
     @GetMapping
-    public String events(Model model) {
-        //List<String> events = new ArrayList<String>();
-        //events.add("Coding Land");
-        //events.add("Stack'em High");
-        //events.add("404 But Not Atlanta");
-        //String events = "<ol>" + "<li>'Coding Land'</li>" + "<li>'Stack'em High'</li>" + "</ol>";
+    public String displayAllEvents(Model model) {
+        model.addAttribute("title", "All Events");
         model.addAttribute("events", events);
         return "events/index";
     }
 
     @GetMapping("create")
-    public String renderCreateEventForm() {
+    public String displayCreateEventForm(Model model) {
+        model.addAttribute("title", "Create Event");
         return "events/create";
     }
 
     @PostMapping("create")
-    public String processCreateFormSubmission(@RequestParam String eventName) {
+    public String processCreateEventForm(@RequestParam String eventName) {
         events.add(eventName);
-        return "redirect:/events";
+        return "redirect:";
     }
-
-
 
 }
